3285112396037a3d7696f5707cb46d6a9ff3ecd2
isaacW <ISAAC.WILSON1811@STU.MTEC.EDU>
2021-03-16 07:42:47 -0600

    commented out most html focus on getting relevent data from api

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-init();
-async function init(){
-    const getCoursesFromAPI = await fetch('https://golf-courses-api.herokuapp.com/courses',
-    {method: 'GET' , headers: {ContentType: 'application/json'}});
-    let response = await getCoursesFromAPI.json();
-    let data = await response;
-    renderCoursesInfo(data.courses);
-}
+//Get Data For All Courses
+getDataAllCourses()
 
-function renderCoursesInfo(courses){
-    console.log(courses);
+async function getDataAllCourses(){
+    const callAPI = await fetch('https://golf-courses-api.herokuapp.com/courses',
+    {method: 'GET' , headers: {ContentType: 'application/json'}});
+    const json = await callAPI.json();
+    const dump = await json;
+    displayAllCourses(dump.courses);
+};
+
+// Display the All Courses Data
+function displayAllCourses(courses){
+    // console.log(courses);
+    let openHTML = '<div style="display:flex;flex-direction:column;">'
+    let closeHTML = '</div>';
     let textBuffer = '';
     courses.forEach(course=>{
         let code = `
-            <div style="color:white;background:black;">
+            <div style="color:white;background:black;margin:1em;">
             <h1>${course.name}</h1>
             <img onclick="selectCourse('${course.id}')" src="${course.image}"/>
             <p>${course.id}</p>
@@ -20,46 +25,73 @@ function renderCoursesInfo(courses){
         `;
         textBuffer += code;
     })
-    document.getElementById('all_courses_info').innerHTML = textBuffer;
+    document.getElementById('all_courses_info').innerHTML = openHTML+textBuffer+closeHTML;
 }
 
+
+
+// Click Event to selected a course
 async function selectCourse(id){
-    const getCourseFromAPI = await fetch('https://golf-courses-api.herokuapp.com/courses/'+id,
-    {method: 'GET' , headers: {ContentType: 'application/json'}});
-    let response = await getCourseFromAPI.json();
-    let data = await response;
-    renderSelectedCourse(data.data);
+    const callAPI = await fetch('https://golf-courses-api.herokuapp.com/courses/'+id);
+    const json = await callAPI.json();
+    const dump = await json;
+    extractSelectedCourseData(dump.data);
 }
 
-function renderSelectedCourse(data){
-    console.log(data);
-    let textBuffer = '';
-    for(let key in data){
-        console.log(key, data[key]);
-        if (!Array.isArray(data[key])){
-            let code = `
-                <div style="color:black;background:white;">
-                    <p>${key}: ${data[key]}</p>
-                </div>
-            `;
-            textBuffer += code;
-        } else {
-            data.holes.forEach(hole=>{
-                for(let prop in hole){
-                    let code =`
-                    <div style="color:black;background:white;">
-                        <p>${prop}: ${hole[prop]}</p>
-                    </div>
-                    `;
-                    textBuffer += code;
-                }
-            })
-        }
+// extract the needed information
+function extractSelectedCourseData(obj){
+    // console.log(obj);
+    let selectedCourse = new Course(obj);
+    console.log(selectedCourse);
+
+}
+
+class Course {
+    constructor(obj){
+        this.name = obj.name;
+        this.id = obj.courseId;
+        this.holeCount = obj.holeCount;
+        this.holes = Course.extractHoles(obj.holes);
     }
-    document.getElementById('selected_course_info').innerHTML = textBuffer;
+    static extractHoles(array){
+        return array;
+    }
 }
 
+    // let holesData = []
+    // let holesArr = data.holes;
+    // let yardsArr = [];
+    // let parArr = [];
+    // for (let i = 0; i < holesArr.length; i++){
+    //     yardsArr.push(holesArr[i].teeBoxes[0].yards);
+    //     parArr.push(holesArr[i].teeBoxes[0].par);
+    // }
+    // displayHolesData(yardsArr,parArr);
 
+    // let textBuffer = '';
+    // for(let key in data){
+    //     console.log(key, data[key]);
+    //     if (!Array.isArray(data[key])){
+    //         let code = `
+    //             <div style="color:black;background:white;">
+    //                 <p>${key}: ${data[key]}</p>
+    //             </div>
+    //         `;
+    //         textBuffer += code;
+    //     } else {
+    //         data.holes.forEach(hole=>{
+    //             for(let prop in hole){
+    //                 let code =`
+    //                 <div style="color:black;background:white;">
+    //                     <p>${prop}: ${hole[prop]}</p>
+    //                 </div>
+    //                 `;
+    //                 textBuffer += code;
+    //             }
+    //         })
+    //     }
+    // }
+    // document.getElementById('selected_course_info').innerHTML = textBuffer;
 
 
 
